35e3bd6000db61a9bc23ee67e8a8503beb4f7c28
laurent <laurent@email.com>
2019-11-17 16:19:33 +0100

    Initial revision

---
+++ CBV_FuelCalc.py
@@ -0,0 +1,396 @@
+version = "0.4"
+
+import ac, acsys, platform, os, sys, time, re, configparser, traceback, random, math
+
+try:
+    if platform.architecture()[0] == "64bit":
+        sysdir = os.path.dirname(__file__)+'/lib/stdlib64'
+    else:
+        sysdir = os.path.dirname(__file__)+'/lib/stdlib'
+
+    sys.path.insert(0, sysdir)
+    os.environ['PATH'] = os.environ['PATH'] + ";."
+except Exception as e:
+    debug(repr(traceback.format_exception(exc_type, exc_value, exc_traceback)))
+
+from lib.sim_info_cbv import sm
+import threading
+import ctypes
+from ctypes import wintypes
+
+## variables
+
+# app
+
+appName = "CBV_FuelCalc"
+mainApp = 0
+mainAppIsActive = False
+x_app_size = 300
+y_app_size = 210
+defaultFontSize = 14
+customFontName = "Digital-7 Mono"
+backgroundOpacity = 0
+
+# app related
+
+timer = 0
+formTimer = 0
+waitingForSessionType = True
+previousSessionType = -1
+averageFuelPerLapValue = 0
+raceFuelNeededValue = 0
+raceTotalLapsText = 0
+raceTotalLapsValue = 0
+bestLapTimeText = 0
+bestLapTimeValue = 0
+
+# fuel
+
+averageFuelPerLap = 0.0
+fuelLastLap = 0.0
+completedLaps = 0.0
+fuelAtLapStart = 0.0
+distanceTraveledAtStart = 0.0
+fuelAtStart = 0.0
+lastFuelMeasurement = 0.0
+lastDistanceTraveled = 0.0
+fuelLapsCounted = 0
+fuelUsedForCountedLaps = 0.0
+
+# config
+
+settingsSectionGeneral = "GENERAL"
+settingsSectionFUELCALC = "FUELCALC"
+extraLiters = 2
+isTimedRace = True
+timedRaceCheckbox = 0
+timedRaceMinutesSpinner = 0
+timedRaceMinutes = 20
+timedRaceExtraLaps = 0
+timedRacePlusLapButton = 0
+timedRaceMinLapButton = 0
+raceLapsSpinner = 0
+raceLaps = 10
+resetButton = 0
+percentOfBestLapTime = 1.07
+
+def acMain(ac_version):
+    global version, appName, mainApp, x_app_size, y_app_size, backgroundOpacity, customFontName
+
+    try:
+        debug("starting version " + version)
+
+        mainApp = ac.newApp(appName)
+        ac.setTitle(mainApp, "")
+        ac.drawBorder(mainApp, 0)
+        ac.setIconPosition(mainApp, 0, -10000)
+        ac.setBackgroundOpacity(mainApp, backgroundOpacity)
+        # ac.initFont(0, customFontName, 0, 0)
+        ac.setSize(mainApp, x_app_size, y_app_size)
+        ac.addOnAppActivatedListener(mainApp, onMainAppActivatedListener)
+        ac.addOnAppDismissedListener(mainApp, onMainAppDismissedListener)
+        ac.addRenderCallback(mainApp, onMainAppFormRender)
+
+        getSettings()
+        createUI()
+
+        return appName
+
+    except exception:
+        debug(repr(traceback.format_exception(exc_type, exc_value, exc_traceback)))
+        showMessage("Error: " + traceback.format_exc())
+
+def getSettings():
+    global isTimedRace, timedRaceCheckbox, timedRaceMinutesSpinner, timedRaceMinutes, raceLapsSpinner, raceLaps, percentOfBestLapTime
+
+    try:
+        settingsFilePath = os.path.dirname(__file__)+'/settings/settings.ini'
+        if not os.path.isfile(settingsFilePath):
+            with open(settingsFilePath, "w") as sf:
+                debug("Settings file created")
+
+        settingsParser = configparser.ConfigParser()
+        settingsParser.optionxform = str
+        settingsParser.read(settingsFilePath)
+
+        defaultFontSize = int(getSettingsValue(settingsParser, settingsSectionGeneral, "defaultFontSize", 14))
+        extraLiters = int(getSettingsValue(settingsParser, settingsSectionFUELCALC, "extraLiters", 2))
+        isTimedRace = getSettingsValue(settingsParser, settingsSectionFUELCALC, "isTimedRace", True, True)
+        timedRaceMinutes = int(getSettingsValue(settingsParser, settingsSectionFUELCALC, "timedRaceMinutes", 20))
+        raceLaps = int(getSettingsValue(settingsParser, settingsSectionFUELCALC, "raceLaps", 10))
+        percentOfBestLapTime = float(getSettingsValue(settingsParser, settingsSectionFUELCALC, "percentOfBestLapTime", 1.07))
+
+        with open(settingsFilePath, 'w') as settingsFile:
+             settingsParser.write(settingsFile)
+        return True
+
+    except exception:
+        debug("Error while loading settings : " + traceback.format_exc())
+        return False
+
+def createUI():
+    global x_app_size, y_app_size, defaultFontSize
+    global isTimedRace, timedRaceCheckbox, averageFuelPerLapValue, raceFuelNeededValue, raceTotalLapsText, raceTotalLapsValue, bestLapTimeText, bestLapTimeValue, timedRaceMinutesSpinner, raceLapsSpinner, resetButton, timedRacePlusLapButton, timedRaceMinLapButton
+
+    try:
+        row = 0
+        x_offset = 5
+        y_offset = 20
+
+        createLabel("averageFuelPerLapText", "Avg fuel per lap : ", x_offset, row * y_offset, defaultFontSize, "left")
+        averageFuelPerLapValue = createLabel("averageFuelPerLapValue", "--", x_app_size - x_offset, row * y_offset, defaultFontSize, "right")
+        row += 1
+        createLabel("raceFuelNeededText", "Race fuel needed : ", x_offset, row * y_offset, defaultFontSize, "left")
+        raceFuelNeededValue = createLabel("raceFuelNeededValue", "--", x_app_size - x_offset, row * y_offset, defaultFontSize, "right")
+        row += 1
+        createLabel("timedRaceLabel", "Is timed race : ", x_offset, row * y_offset, defaultFontSize, "left")
+        timedRaceCheckbox = ac.addCheckBox(mainApp, "")
+        ac.setPosition(timedRaceCheckbox, x_app_size - (x_offset + 15), row * y_offset + 4)
+        ac.setSize(timedRaceCheckbox, 15, 15)
+        ac.setValue(timedRaceCheckbox, isTimedRace)
+        ac.addOnCheckBoxChanged(timedRaceCheckbox, onTimedRaceChangedListener)
+        row += 1
+
+        raceTypeRow = row
+
+        raceTotalLapsText = createLabel("raceTotalLapsText", "Expected race laps : ", x_offset, row * y_offset, defaultFontSize, "left")
+        raceTotalLapsValue = createLabel("raceTotalLapsValue", "--", x_app_size - x_offset, row * y_offset, defaultFontSize, "right")
+        timedRaceMinLapButton = ac.addButton(mainApp, "-")
+        ac.setPosition(timedRaceMinLapButton, 180, row * y_offset)
+        ac.setSize(timedRaceMinLapButton, 20, 20)
+        ac.addOnClickedListener(timedRaceMinLapButton, onTimedRaceMinLapButtonClickedListener)
+        timedRacePlusLapButton = ac.addButton(mainApp, "+")
+        ac.setPosition(timedRacePlusLapButton, 205, row * y_offset)
+        ac.setSize(timedRacePlusLapButton, 20, 20)
+        ac.addOnClickedListener(timedRacePlusLapButton, onTimedRacePlusLapButtonClickedListener)
+        row += 1
+        bestLapTimeText = createLabel("bestLapTimeText", "Best lap time : ", x_offset, row * y_offset, defaultFontSize, "left")
+        bestLapTimeValue = createLabel("bestLapTimeValue", "--", x_app_size - x_offset, row * y_offset, defaultFontSize, "right")
+        row += 3
+        timedRaceMinutesSpinner = ac.addSpinner(mainApp, "Timed race minutes")
+        ac.setRange(timedRaceMinutesSpinner, 0, 240)
+        ac.setValue(timedRaceMinutesSpinner, timedRaceMinutes)
+        ac.setStep(timedRaceMinutesSpinner, 1)
+        ac.setPosition(timedRaceMinutesSpinner, x_app_size / 6, row * y_offset)
+        ac.addOnValueChangeListener(timedRaceMinutesSpinner, onTimedRaceMinutesChangedListener)
+        row += 2
+        resetButton = ac.addButton(mainApp, "Reset")
+        ac.setPosition(resetButton, x_offset, row * y_offset)
+        ac.setSize(resetButton, 50, 22)
+        ac.addOnClickedListener(resetButton, onResetClickedListener)
+
+        row = raceTypeRow
+
+        row += 2
+        raceLapsSpinner = ac.addSpinner(mainApp, "Race laps")
+        ac.setRange(raceLapsSpinner, 0, 100)
+        ac.setValue(raceLapsSpinner, raceLaps)
+        ac.setStep(raceLapsSpinner, 1)
+        ac.setPosition(raceLapsSpinner, x_app_size / 6, row * y_offset)
+        ac.addOnValueChangeListener(raceLapsSpinner, onRaceLapsChangedListener)
+
+        updateUIVisibility()
+
+    except exception:
+        debug(repr(traceback.format_exception(exc_type, exc_value, exc_traceback)))
+        showMessage("Error: " + traceback.format_exc())
+
+def updateUIVisibility():
+    global isTimedRace, raceTotalLapsText, raceTotalLapsValue, bestLapTimeText, bestLapTimeValue, timedRaceMinutesSpinner, raceLapsSpinner
+
+    ac.setVisible(raceTotalLapsText, isTimedRace)
+    ac.setVisible(raceTotalLapsValue, isTimedRace)
+    ac.setVisible(bestLapTimeText, isTimedRace)
+    ac.setVisible(bestLapTimeValue, isTimedRace)
+    ac.setVisible(timedRaceMinutesSpinner, isTimedRace)
+    ac.setVisible(raceLapsSpinner, not isTimedRace)
+
+def onMainAppActivatedListener(*args):
+    global mainAppIsActive
+
+    mainAppIsActive = True
+
+    debug("Main app is active")
+
+def onMainAppDismissedListener(*args):
+    global mainAppIsActive
+
+    mainAppIsActive = False
+
+    debug("Main app is inactive")
+
+def onMainAppFormRender(delta):
+    global formTimer, waitingForSessionType, previousSessionType, waitTimeSessionType, timerSessionType
+
+    formTimer += deltaT
+    if formTimer < 0.050:
+        return
+
+    formTimer = 0
+
+    if waitingForSessionType:
+        session = sm.graphics.session
+        if previousSessionType != session:
+            waitingForSessionType = False
+
+def acUpdate(deltaT):
+    global mainAppIsActive, timer
+    global averageFuelPerLap, fuelLastLap, completedLaps, fuelAtLapStart, distanceTraveledAtStart, fuelAtStart, lastFuelMeasurement, lastDistanceTraveled, fuelLapsCounted, fuelUsedForCountedLaps, percentOfBestLapTime
+
+    timer += deltaT
+    if timer < 0.025:
+        return
+
+    timer = 0
+
+    try:
+        remaining = sm.physics.fuel
+        currentLap = sm.graphics.completedLaps
+        totalLaps = sm.graphics.numberOfLaps
+        distanceTraveled = sm.graphics.distanceTraveled
+        isInPit = sm.graphics.isInPit
+        inPitLane = ac.isCarInPitline(0)
+        maxFuel = sm.static.maxFuel
+        normalizedSplinePosition = round(sm.graphics.normalizedCarPosition, 4)
+        bestLapTime = sm.graphics.iBestTime
+        lastLapTime = sm.graphics.iLastTime
+
+        if currentLap != completedLaps: #when crossed finish line
+            debug("Current Lap %d, fuelAtLapStart = %.1f, remaining = %.1f" % (currentLap, fuelAtLapStart, remaining))
+
+            if currentLap >= 2 and fuelAtLapStart > remaining: #if more than 2 laps driven
+                thresholdLapTime = bestLapTime * percentOfBestLapTime
+
+                debug("lastLapTime = %.3f, thresholdLapTime = %.3f" % (lastLapTime, thresholdLapTime))
+
+                if lastLapTime > thresholdLapTime:
+                    debug("Last lap was too slow, fuel not counted")
+                else:
+                    fuelLastLap = fuelAtLapStart - remaining # calculate fuel used last lap
+                    fuelLapsCounted += 1
+                    fuelUsedForCountedLaps += fuelLastLap
+                    # averageFuelPerLap = (averageFuelPerLap * (currentLap - 2) + fuelLastLap) / (currentLap - 1) #calculate AverageFuelPerLap
+                    averageFuelPerLap = fuelUsedForCountedLaps / fuelLapsCounted
+                    debug("AverageFuelPerLap = %.3f" % (averageFuelPerLap))
+                    updateFuelEstimate()
+
+            fuelAtLapStart = remaining #reset fuelAtLapStart
+            completedLaps = currentLap #set completedLaps
+
+            debug("fuelLapsCounted %d, fuelUsedForCountedLaps = %.1f" % (fuelLapsCounted, fuelUsedForCountedLaps))
+
+    except exception:
+        debug(repr(traceback.format_exception(exc_type, exc_value, exc_traceback)))
+        showMessage("Error: " + traceback.format_exc())
+
+def updateFuelEstimate():
+    global averageFuelPerLap, timedRaceMinutes, extraLiters, timedRaceExtraLaps, isTimedRace, raceLaps
+    global averageFuelPerLapValue, raceFuelNeededValue, raceTotalLapsValue
+
+    if averageFuelPerLap != 0.0:
+        bestLapTime = sm.graphics.iBestTime
+        raceTime = timedRaceMinutes * 60 * 1000
+        if isTimedRace:
+            laps = (raceTime / bestLapTime) + timedRaceExtraLaps
+        else:
+            laps = raceLaps
+        fuelNeeded = math.ceil(math.ceil(laps) * averageFuelPerLap) + extraLiters
+        bestLapValueSeconds = (bestLapTime / 1000) % 60
+        bestLapValueMinutes = (bestLapTime // 1000) // 60
+
+        ac.setText(averageFuelPerLapValue, "%.3f" % (averageFuelPerLap))
+        if timedRaceExtraLaps != 0:
+            if timedRaceExtraLaps > 0:
+                ac.setText(raceTotalLapsValue, "%.1f (+%d)" % (laps, timedRaceExtraLaps))
+            else:
+                ac.setText(raceTotalLapsValue, "%.1f (%d)" % (laps, timedRaceExtraLaps))
+        else:
+            ac.setText(raceTotalLapsValue, "%.1f" % (laps))
+        ac.setText(raceFuelNeededValue, str(fuelNeeded))
+        ac.setText(bestLapTimeValue,  "{:.0f}:{:06.3f}".format(bestLapValueMinutes, bestLapValueSeconds)[:-1])
+    else:
+        ac.setText(averageFuelPerLapValue, "--")
+        ac.setText(raceTotalLapsValue, "--")
+        ac.setText(raceFuelNeededValue, "--")
+        ac.setText(bestLapTimeValue,  "--")
+
+def onTimedRaceChangedListener(*args):
+    global isTimedRace, timedRaceCheckbox
+
+    if isTimedRace:
+        isTimedRace = False
+    else:
+        isTimedRace = True
+
+    updateUIVisibility()
+    updateFuelEstimate()
+
+def onTimedRaceMinutesChangedListener(*args):
+    global timedRaceMinutesSpinner, timedRaceMinutes
+
+    timedRaceMinutes = ac.getValue(timedRaceMinutesSpinner)
+    updateFuelEstimate()
+
+def onTimedRaceMinLapButtonClickedListener(*args):
+    global timedRaceExtraLaps
+
+    timedRaceExtraLaps -= 1
+    updateFuelEstimate()
+
+def onTimedRacePlusLapButtonClickedListener(*args):
+    global timedRaceExtraLaps
+
+    timedRaceExtraLaps += 1
+    updateFuelEstimate()
+
+def onRaceLapsChangedListener(*args):
+    global raceLapsSpinner, raceLaps
+
+    raceLaps = ac.getValue(raceLapsSpinner)
+    updateFuelEstimate()
+
+def onResetClickedListener(*args):
+    global averageFuelPerLap, fuelLapsCounted, fuelUsedForCountedLaps
+
+    averageFuelPerLap = 0.0
+    fuelLapsCounted = 0
+    fuelUsedForCountedLaps = 0.0
+
+    updateFuelEstimate()
+
+def acShutdown(*args):
+    global mainAppIsActive
+
+def createLabel(name, text, x, y, font_size = 14, align = "center"):
+    global mainApp, labels
+
+    label = ac.addLabel(mainApp, name)
+    ac.setText(label, text)
+    ac.setPosition(label, x, y)
+    ac.setFontSize(label, font_size)
+    ac.setFontAlignment(label, align)
+
+    return label
+
+def getSettingsValue(parser, section, option, value, boolean = False):
+    if parser.has_option(str(section), str(option)):
+        if boolean:
+            return parser.getboolean(str(section), option)
+        else:
+            return parser.get(str(section), str(option))
+    else:
+        return setSettingsValue(parser, section, option, value)
+
+
+def setSettingsValue(parser, section, option, value):
+    if not parser.has_section(section):
+        parser.add_section(section)
+
+    parser.set(str(section), str(option), str(value))
+    return value
+
+def debug(message):
+    global appName
+
+    ac.log(appName + ": " + message)
+    ac.console(appName + ": " + message)
---
+++ lib/sim_info_cbv.py
@@ -0,0 +1,249 @@
+"""
+Assetto Corsa shared memory for Python applications
+
+_ctypes.pyd must be somewhere in sys.path, because AC doesn't include all Python binaries.
+
+Usage. Let's say you have following folder structure::
+
+    some_app
+        DLLs
+            _ctypes.pyd
+        some_app.py
+
+some_app.py::
+
+    import os
+    import sys
+    sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'DLLs'))
+
+    from sim_info import info
+
+    print(info.graphics.tyreCompound, info.physics.rpms, info.static.playerNick)
+
+
+Do whatever you want with this code!
+WBR, Rombik :)
+"""
+import mmap
+import os
+import struct
+import functools
+import ctypes
+from ctypes import c_int32, c_float, c_wchar,  c_char
+
+AC_STATUS = c_int32
+AC_OFF = 0
+AC_REPLAY = 1
+AC_LIVE = 2
+AC_PAUSE = 3
+AC_SESSION_TYPE = c_int32
+AC_UNKNOWN = -1
+AC_PRACTICE = 0
+AC_QUALIFY = 1
+AC_RACE = 2
+AC_HOTLAP = 3
+AC_TIME_ATTACK = 4
+AC_DRIFT = 5
+AC_DRAG = 6
+
+# Added on Oct 26, 2015
+AC_FLAG_TYPE = c_int32
+AC_NO_FLAG = 0
+AC_BLUE_FLAG = 1
+AC_YELLOW_FLAG = 2
+AC_BLACK_FLAG = 3
+AC_WHITE_FLAG = 4
+AC_CHECKERED_FLAG = 5
+AC_PENALTY_FLAG = 6
+
+
+class SPageFilePhysicsCBV_FuelCalc(ctypes.Structure):
+    _pack_ = 4
+    _fields_ = [
+        ('packetId', c_int32),
+        ('gas', c_float),
+        ('brake', c_float),
+        ('fuel', c_float),
+        ('gear', c_int32),
+        ('rpms', c_int32),
+        ('steerAngle', c_float),
+        ('speedKmh', c_float),
+        ('velocity', c_float * 3),
+        ('accG', c_float * 3),
+        ('wheelSlip', c_float * 4),
+        ('wheelLoad', c_float * 4),
+        ('wheelsPressure', c_float * 4),
+        ('wheelAngularSpeed', c_float * 4),
+        ('tyreWear', c_float * 4),
+        ('tyreDirtyLevel', c_float * 4),
+        ('tyreCoreTemperature', c_float * 4),
+        ('camberRAD', c_float * 4),
+        ('suspensionTravel', c_float * 4),
+        ('drs', c_float),
+        ('tc', c_float),
+        ('heading', c_float),
+        ('pitch', c_float),
+        ('roll', c_float),
+        ('cgHeight', c_float),
+        ('carDamage', c_float * 5),
+        ('numberOfTyresOut', c_int32),
+        ('pitLimiterOn', c_int32),
+        ('abs', c_float),
+
+        # Added on Oct 26, 2015
+        ('kersCharge', c_float),
+        ('kersInput', c_float),
+        ('autoShifterOn', c_int32),
+        ('rideHeight', c_float * 2),
+
+		# Added on Mar 31, 2016
+		('turboBoost', c_float),
+		('ballast', c_float),
+		('airDensity', c_float),
+
+		# Added on May 18, 2016
+		('airTemp', c_float),
+		('roadTemp', c_float),
+
+        # More info is availiable but not needed in this app
+
+    ]
+
+
+class SPageFileGraphicCBV_FuelCalc(ctypes.Structure):
+    _pack_ = 4
+    _fields_ = [
+        ('packetId', c_int32),
+        ('status', AC_STATUS),
+        ('session', AC_SESSION_TYPE),
+        ('currentTime', c_wchar * 15),
+        ('lastTime', c_wchar * 15),
+        ('bestTime', c_wchar * 15),
+        ('split', c_wchar * 15),
+        ('completedLaps', c_int32),
+        ('position', c_int32),
+        ('iCurrentTime', c_int32),
+        ('iLastTime', c_int32),
+        ('iBestTime', c_int32),
+        ('sessionTimeLeft', c_float),
+        ('distanceTraveled', c_float),
+        ('isInPit', c_int32),
+        ('currentSectorIndex', c_int32),
+        ('lastSectorTime', c_int32),
+        ('numberOfLaps', c_int32),
+        ('tyreCompound', c_wchar * 33),
+
+        ('replayTimeMultiplier', c_float),
+        ('normalizedCarPosition', c_float),
+        ('carCoordinates', c_float * 3),
+
+        # Added on Oct 26, 2015
+        ('penaltyTime', c_float),
+        ('flag', AC_FLAG_TYPE),
+        ('idealLineOn', c_int32),
+
+		# Added on Mar 31, 2016
+		('isInPitLine', c_int32),
+		('surfaceGrip', c_float),
+
+        #added May 2017
+		('MandatoryPitDone', c_int32),
+    ]
+
+
+class SPageFileStaticCBV_FuelCalc(ctypes.Structure):
+    _pack_ = 4
+    _fields_ = [
+        ('_smVersion', c_wchar * 15),
+        ('_acVersion', c_wchar * 15),
+        # session static info
+        ('numberOfSessions', c_int32),
+        ('numCars', c_int32),
+        ('carModel', c_wchar * 33),
+        ('track', c_wchar * 33),
+        ('playerName', c_wchar * 33),
+        ('playerSurname', c_wchar * 33),
+        ('playerNick', c_wchar * 33),
+        ('sectorCount', c_int32),
+
+        # car static info
+        ('maxTorque', c_float),
+        ('maxPower', c_float),
+        ('maxRpm', c_int32),
+        ('maxFuel', c_float),
+        ('suspensionMaxTravel', c_float * 4),
+        ('tyreRadius', c_float * 4),
+
+		#Added on Mar 31, 2016
+		('maxTurboBoost', c_float),
+		('airTemp', c_float),
+		('roadTemp', c_float),
+		('penaltiesEnabled', c_int32),
+		('aidFuelRate', c_float),
+		('aidTireRate', c_float),
+		('aidMechanicalDamage', c_float),
+		('aidAllowTyreBlankets', c_int32),
+		('aidStability', c_float),
+		('aidAutoClutch', c_int32),
+		('aidAutoBlip', c_int32),
+
+        #added May 2017
+        ('hasDRS', c_int32),
+        ('hasERS', c_int32),
+        ('hasKERS', c_int32),
+        ('kersMaxJ', c_float),
+        ('engineBrakeSettingsCount', c_int32),
+        ('ersPowerControllerCount', c_int32),
+        ('trackSPlineLength', c_float),
+        ('​trackConfiguration', c_wchar * 33),
+        ('ersMaxJ', c_float),
+        ('isTimedRace', c_int32),
+        ('hasExtraLap', c_int32),
+        ('carSkin', c_wchar * 33),
+        ('reversedGridPositions', c_int32),
+        ('PitWindowStart', c_int32),
+        ('PitWindowEnd', c_int32),
+    ]
+
+
+class CBV_FuelCalcShared:
+    def __init__(self):
+        self._acpmf_physics_cbv_fuelcalc = mmap.mmap(0, ctypes.sizeof(SPageFilePhysicsCBV_FuelCalc), "acpmf_physics")
+        self._acpmf_graphics_cbv_fuelcalc = mmap.mmap(0, ctypes.sizeof(SPageFileGraphicCBV_FuelCalc), "acpmf_graphics")
+        self._acpmf_static_cbv_fuelcalc = mmap.mmap(0, ctypes.sizeof(SPageFileStaticCBV_FuelCalc), "acpmf_static")
+        self.physics = SPageFilePhysicsCBV_FuelCalc.from_buffer(self._acpmf_physics_cbv_fuelcalc)
+        self.graphics = SPageFileGraphicCBV_FuelCalc.from_buffer(self._acpmf_graphics_cbv_fuelcalc)
+        self.static = SPageFileStaticCBV_FuelCalc.from_buffer(self._acpmf_static_cbv_fuelcalc)
+
+    def close(self):
+        self._acpmf_physics_cbv_fuelcalc.close()
+        self._acpmf_graphics_cbv_fuelcalc.close()
+        self._acpmf_static_cbv_fuelcalc.close()
+
+    def __del__(self):
+        self.close()
+
+sm = CBV_FuelCalcShared()
+
+def demo():
+    import time
+
+    for _ in range(400):
+        print(sm.static.track, sm.graphics.tyreCompound, sm.graphics.currentTime,
+              sm.physics.rpms, sm.graphics.currentTime, sm.static.maxRpm, list(sm.physics.tyreWear))
+        time.sleep(0.1)
+
+
+def do_test():
+    for struct in sm.static, sm.graphics, sm.physics:
+        print(struct.__class__.__name__)
+        for field, type_spec in struct._fields_:
+            value = getattr(struct, field)
+            if not isinstance(value, (str, float, int)):
+                value = list(value)
+            print(" {} -> {} {}".format(field, type(value), value))
+
+
+if __name__ == '__main__':
+    do_test()
+    demo()
